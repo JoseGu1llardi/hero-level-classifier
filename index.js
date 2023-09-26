9621029c21c0976516082ddf5ed394aa44250dab
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2023-09-26 17:06:29 +0100

    added code to rank hero levels

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("");
+let heroName = "Guillardi";
+let xp = 6000;
+
+const levels = {
+  Iron: 1000,
+  Brass: 2000,
+  Silver: 5000,
+  Gold: 7000,
+  Platinum: 8000,
+  Upward: 9000,
+  Imortal: 10000,
+  Radiant: 10001,
+};
+
+function getLevel(xp) {
+  for (const levelName of Object.keys(levels)) {
+    if (xp <= levels[levelName]) return levelName;
+  }
+
+  return "Unknown level ";
+}
+
+const level = getLevel(xp);
+
+console.log(`The Hero named ${heroName} is at the level of ${level}`);
+
+console.log(levels["Gold"]);
